6316002c7c2f3da319690342ed27ac6734f9658e
alexey <songuitar@yandex.ru>
2023-12-15 18:27:54 +0300

    added context menu

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -2,27 +2,57 @@
 
 import {useTree} from "./hooks/data-fetching";
 import {Employee} from "@organization-tree/api-interfaces";
+import {Item, Menu, useContextMenu} from "react-contexify";
+import "react-contexify/dist/ReactContexify.css";
 
 
-function renderTree(tree: Employee[]) {
+function renderTree(tree: Employee[], lvl: number = 0) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { show } = useContextMenu({
+    id: 'Menu',
+  });
+
+
   return (
-    <ul className="tree">
-      {tree?.map(employee => (
-        <li key={employee.id}>
-          <code>{employee.name}</code>
-          { renderTree(employee.subordinates)}
-        </li>
-      ))}
-    </ul>
+      <ul className={lvl === 0 ? "tree" : ''}>
+        {tree?.map(employee => (
+          <li key={employee.id} onClick={(event) => {
+            event.stopPropagation()
+
+            show({
+              event,
+              props: {
+                employeeId: employee.id
+              }
+            })
+          }}>
+            <code>{employee.name + " (" + employee.id + ")"}</code>
+            { renderTree(employee.subordinates, lvl + 1)}
+          </li>
+        ))}
+      </ul>
   )
 }
 
 
 export default function Index() {
 
-   const { tree, isLoading, isError } = useTree()
+  const { tree, isLoading, isError } = useTree()
+
+  function remove({ event, props }){
+    console.log(props);
+  }
+  function changeBoss({ event, props }){
+    console.log(props);
+  }
 
   return (
-    renderTree(tree)
+    <>
+      {renderTree(tree)}
+      <Menu id='Menu'>
+        <Item onClick={remove}>Remove</Item>
+        <Item onClick={changeBoss}>Change Boss</Item>
+      </Menu>
+    </>
   );
 }
